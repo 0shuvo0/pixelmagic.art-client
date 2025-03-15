5c7f07a85590aa5205d6ace18541968069d8551c
0shuvo0 <seriouslolz1@gmail.com>
2025-03-15 13:51:09 +0600

    index page

---
+++ src/scripts/index.js
@@ -0,0 +1,7 @@
+import '../styles/global.scss'
+
+const faqItems = document.querySelectorAll(".faq-item")
+faqItems.forEach(item => {
+    item.querySelector('.faq-nav').addEventListener('click', () => item.classList.toggle('active'))
+    console.log(item.querySelector('.faq-nav'))
+})
